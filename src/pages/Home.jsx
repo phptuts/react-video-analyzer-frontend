9f1d20482c4824f4f9a561b2301a67cf947d9edc
phptuts <glaserpower@gmail.com>
2024-06-27 19:28:35 -0700

    7-login-with-google

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
 const Home = () => {
+  const loginWithGoogle = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <h1>Home</h1>
+      <div className="row">
+        <div className="col">
+          <h1>Home Page</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button onClick={loginWithGoogle} className="btn btn-success">
+            Login With Google
+          </button>
+        </div>
+      </div>
     </>
   );
 };
