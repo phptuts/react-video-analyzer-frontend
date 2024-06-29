bc1f6c2c754749a32ad9b8f120f8f962c84de6a8
phptuts <glaserpower@gmail.com>
2024-06-28 19:01:24 -0700

    12-create-submitting-form-state

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Upload = () => {
+  const [submitting, setSubmitting] = useState(false);
+  if (submitting) {
+    return (
+      <>
+        <div className="row">
+          <div className="col mt-3">
+            <h1>Upload Video</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col mt-3">
+            <h2 className="text-center">
+              Your video is being upload to our site, please do not leave the
+              page...
+            </h2>
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Upload</h1>
+      <div className="row">
+        <div className="col mt-3">
+          <h1>Upload Video</h1>
+        </div>
+      </div>
     </>
   );
 };
