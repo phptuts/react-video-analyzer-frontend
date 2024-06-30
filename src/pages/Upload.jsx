e5888ba945ef004f324a6c661c9623d8c8dc1b8d
phptuts <glaserpower@gmail.com>
2024-06-29 19:29:10 -0700

    13-roughing-out-the-form

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 
 const Upload = () => {
   const [submitting, setSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    file: null,
+    title: "",
+  });
+  const handleFileChange = (e) => {
+    setFormData({ ...formData, file: e.target.files[0] });
+  };
+
+  const handleTitleChange = (e) => {
+    setFormData({ ...formData, title: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("FORM_SUBMITTED");
+  };
   if (submitting) {
     return (
       <>
@@ -27,6 +43,39 @@ const Upload = () => {
         <div className="col mt-3">
           <h1>Upload Video</h1>
         </div>
+      </div>
+      <div className="row">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              value={formData.title}
+              onChange={handleTitleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="formFile" className="form-label">
+              Upload Video
+            </label>
+            <input
+              type="file"
+              className="form-control"
+              id="formFile"
+              accept="video/*"
+              onChange={handleFileChange}
+            />
+          </div>
+          <div className="mb-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
