5a115f294b2a16da8ea4eba01b93d53a8596c9a4
phptuts <glaserpower@gmail.com>
2024-07-03 21:00:54 -0700

    24-videos-page-loading-use-state

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const Videos = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <h1>Videos</h1>
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3 mb-3 text-center">Your Videos</h1>
+        </div>
+      </div>
+      <div className="row">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Created Date</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
     </>
   );
 };
